29170e75b3bca7abb6c1477989305436acbb9018
Sabrina Haßlocher <62216740+Saccha@users.noreply.github.com>
2022-09-04 21:36:37 -0300

    Add files via upload

---
+++ static/js/models.js
@@ -0,0 +1,80 @@
+let modelsJson = [
+  {
+    id: 1,
+    name: "Rucula",
+    img: "../static/img/Rucula.jpg",
+    price: [4.65, 6.65, 13.95],
+    sizes: ["300g", "600g", "900g"],
+    description:
+      "É uma planta comestível usualmente usada em saladas e pode ser encontrada no meio natural em zonas úmidas como rios e riachos",
+  },
+
+  {
+    id: 2,
+    name: "Melancia",
+    img: "../static/img/melancia.jpg",
+    price: [32.2, 48.3, 96.6],
+    sizes: ["4,66Kg", "7Kg", "14Kg"],
+    description:
+      "A melancia é uma fruta cheia de nutrientes que proporciona variados benefícios á saúde. Rica em vitamina A, betacaroteno, vitaminas do complexo B, vitamina C, fibras, ferro, potássio e magnésio além do licopeno que ajuda a prevenir o câncer.",
+  },
+
+  {
+    id: 3,
+    name: "Banana",
+    img: "../static/img/banana.jpg",
+    price: [4.95, 9.9, 14, 85],
+    sizes: ["300g", "600g", "900g"],
+    description:
+      "É a fruta mais popular do Brasil e um dos símbolos de países tropicais, A oferta de fibras no alimento promove sensação de saciedade e diminui a absorção de gorduras saturadas pelo intestino, controlando os níveis de colesterol. ",
+  },
+
+  {
+    id: 4,
+    name: "Alface-roxa",
+    img: "../static/img/alfaceroxo.jpg",
+    price: [4.45, 8.9, 13.35],
+    sizes: ["120g", "240g", "460"],
+    description:
+      "A alface roxa tem altos indíces de antioxidantes e antocianina (responsável pela sua coloração), bem maiores do que qualquer outro tipo de alface",
+  },
+
+  {
+    id: 5,
+    name: "Uva",
+    img: "../static/img/uva.jpg",
+    price: [13.9, 27.8, 41.7],
+    sizes: ["500g", "1Kg", "1.5Kg"],
+    description:
+      "Uva fresquinha, direto do produtor para a mesa da sua casa. Elas são docinhas e cheias de nutrientes. A família toda vai adorar!",
+  },
+
+  {
+    id: 6,
+    name: "Agrião",
+    img: "../static/img/agriao.jpg",
+    price: [4.42, 8.84, 13, 26],
+    sizes: ["200g", "400g", "600g"],
+    description:
+      "Agrião hidropônico, somente produtos selecionados e frescos. Excelente fonte de vitaminas A, C, B6 e B12, contém doses de sais minerais como ferro e fósforo e magnésio. Suas folhas são bem verdes, tem sabor picante, frequentemente consumido cru em saladas.",
+  },
+
+  {
+    id: 7,
+    name: "Tomate",
+    img: "../static/img/tomate.jpg",
+    price: [4.74, 9.49, 14.23],
+    sizes: ["200g", "400g", "600g"],
+    description: "O mais famosa caça britânico, utilizado em toda a guerra",
+  },
+
+  {
+    id: 8,
+    name: "Maça",
+    img: "../static/img/maca.jpg",
+    price: [3.33, 6.66, 9.99],
+    sizes: ["280g", "560g", "840g"],
+    description:
+      "O mais famoso caça da marinha japonesa, terror do pacífico nos anos iniciais da guerra",
+  },
+];
